34a9d8b568634fbd3d12f41e0de997b5990d4a03
Eduardo França <edox86@gmail.com>
2017-11-29 15:51:32 -0200

    Modificando informações em readme.md

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -36,7 +36,7 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-      $article Article::create($request->all); 
+      $article = Article::create($request->all); 
 
       return response()->json($article, 201);
     }
